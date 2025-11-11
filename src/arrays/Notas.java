e4f0477e86565b6892ecf043eba8e338967c43e5
Liliana <liliana.monterroza18@gmail.com>
2025-11-11 17:54:24 +0100

    ejercicioactualizado

--- src/arrays/Notas.java
+++ src/arrays/Notas.java
@@ -5,13 +5,21 @@ import java.util.Arrays;
 public class Notas {
 
     public static void main(String[] args) {
-        int notaPrimerExamen = 7;
-        int segundoExamen = 6;
 
         int[] notaLiliana = new int[3];
         notaLiliana[0] = 6;
         notaLiliana[1] = 7;
         notaLiliana[2] = 8;
         System.out.println(Arrays.toString(notaLiliana));
+        int[] notasLilianaAuxiliares = notaLiliana;
+        notaLiliana = new int[4];
+        for (int i = 0; i < notasLilianaAuxiliares.length; i++) {
+            notaLiliana[i] = notasLilianaAuxiliares[i]; // bucle para pasar los datos de un array a otro
+
+        }
+        notaLiliana[3] = 7;
+        System.out.println(" notas liliana" + Arrays.toString(notasLilianaAuxiliares));
+        System.out.println( "notas " + Arrays.toString(notaLiliana));
+
     }
 }
