07afac0ab733b09bda1ff852da71caa3919556f2
Liliana <liliana.monterroza18@gmail.com>
2025-11-11 19:25:33 +0100

    arrays ordenados

--- src/arrays/ArrayOrdenado.java
+++ src/arrays/ArrayOrdenado.java
@@ -4,27 +4,27 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class ArrayOrdenado {
-        public static boolean estaOrdenado(int[] array) {
-            for (int i = 0; i < array.length- 1; i++) {
-                if (array[i] > array[i + 1]) {
-                    return false;
-                }
+    public static boolean estaOrdenado(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return  false;
             }
-            return true;
         }
-
-        public static void main(String[] args) {
-
-            Random randi = new Random();
-            int[] numerosAleatorios = new int[20];
-
-            for (int i = 0; i < 20; i++) {
-                numerosAleatorios[i] = randi.nextInt(10);
-
-            }
-            System.out.println("  los numeros aleatorios son" + Arrays.toString(numerosAleatorios ));
-            System.out.println(" el array esta ordenado? " + estaOrdenado(numerosAleatorios));
-        }
+        return true;
     }
+
+    public static void main(String[] args) {
+
+        Random randi = new Random();
+        int[] numerosAleatorios = new int[20];
+
+        for (int i = 0; i < 20; i++) {
+            numerosAleatorios[i] = randi.nextInt(10);
+
+        }
+        System.out.println("  los numeros aleatorios son" + Arrays.toString(numerosAleatorios));
+        System.out.println(" el array esta ordenado? " + estaOrdenado(numerosAleatorios));
+    }
+}
 
 
