4edf8263fb4b077a39d8ea1d303ad1c4dab12cb9
Liliana <liliana.monterroza18@gmail.com>
2025-11-11 17:30:10 +0100

    clases con arrays

---
+++ src/arrays/Notas.java
@@ -0,0 +1,17 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class Notas {
+
+    public static void main(String[] args) {
+        int notaPrimerExamen = 7;
+        int segundoExamen = 6;
+
+        int[] notaLiliana = new int[3];
+        notaLiliana[0] = 6;
+        notaLiliana[1] = 7;
+        notaLiliana[2] = 8;
+        System.out.println(Arrays.toString(notaLiliana));
+    }
+}
