157b5df0d99635826984d2c3469006ac6f07efb8
liliana <liliana.monterroza@gmail.com>
2025-12-09 18:50:40 +0100

    ejercicios con arrays

---
+++ src/arrays/Ejercicio1.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int [] numeros = new int[5];
+        numeros[0]=  7;
+        numeros[1]= 3;
+        numeros[3]= 5;
+        numeros[4]= 9;
+        numeros [5]= 4;
+        System.out.println(numeros[5]);
+
+
+
+    }
+}
---
+++ src/arrays/Ejercicio2.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+
+        String[] dias = new String[3];
+        dias[0]= "lunes";
+        dias[1]= "Martes";
+        dias[2]= "miercoles";
+        System.out.println(dias.toString());
+
+    }
+}
