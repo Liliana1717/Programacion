de52cfc9fbf6994ae2cdc805289543e0d0025177
liliana <liliana.monterroza@gmail.com>
2026-03-11 17:08:13 +0100

    LISTAS NUEVAS

--- src/Lista/Tienda.java
+++ src/Lista/Tienda.java
@@ -2,13 +2,28 @@ package Lista;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Tienda {
 
         private List<VideoJuego> inventario = new ArrayList<>();
         private List<Cliente> clientes = new ArrayList<>();
+        private List<Venta> ventas = new ArrayList<>();
+        public void añadirJuego(){
+            for (VideoJuego v : inventario){
+                System.out.println(v);
+            }
+        }
+   public VideoJuego BuscarPorId(UUID id){
+            for (VideoJuego v : inventario){
+                if (v.getId().equals(id)){
+                    return v;
+                }
 
-
+            }
+            return null;
+   }
+   
 
 
 }
